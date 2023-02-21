536a007cc6c71c75df6c8fb86b42797286658579
Eduard-Osheha <eduardoshega07@gmail.com>
2023-02-21 08:04:45 +0100

    3 commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import debounce from 'lodash.debounce';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
